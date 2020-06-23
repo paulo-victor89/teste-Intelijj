fce276f39b2077e7475c9c989cdb9cea808a1c6e
Paulo Victor <pvictorcampelo@yahoo.com.br>
2020-06-23 17:26:03 -0300

    Initial commit

---
+++ src/pvictor/poo/Main.java
@@ -0,0 +1,11 @@
+package pvictor.poo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Helo word");
+        System.out.println("Testando ");
+
+    }
+}
